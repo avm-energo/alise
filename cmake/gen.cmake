759fc1fa348744c2c94f1ef83fe83630b87f49a7
Anton <vao@asu-vei.ru>
2023-12-20 11:06:38 +0300

    [*] cherry-pick from 90 version of gen version and some small fixes

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -3,13 +3,9 @@ include(FetchContent)
 FetchContent_Declare(
   gen
   GIT_REPOSITORY https://github.com/avm-energo/gen.git
-  GIT_TAG        origin/alice-buster
+  GIT_TAG        353366cbfa7688fa3714db38f7c9f58cd5d87204
 )
 
-#if(BUILD_WITH_ALISE)
-  set(GEN_STATIC ON)
-#else()
-#  set(GEN_STATIC OFF)
-#endif()
+set(GEN_STATIC ON)
 
 FetchContent_MakeAvailable(gen)
