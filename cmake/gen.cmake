d481822840f7f9e088dba84b81a47c33e25efb88
anton <anton@vao>
2024-04-08 11:32:46 +0300

    Update gen.cmake

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   gen
   GIT_REPOSITORY https://github.com/avm-energo/gen.git
-  GIT_TAG        baea5a763413493c523682efd7c1f8b393f4adfc
+  GIT_TAG        origin/main
 )
 
 set(GEN_STATIC ON)
